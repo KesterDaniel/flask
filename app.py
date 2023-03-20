8dca19ac59a3ee644be778d1175bb76f89c419b9
KesterDaniel <kesterdan17@gmail.com>
2023-03-20 21:22:35 +0100

    getting more practice on decorators

--- app.py
+++ app.py
@@ -2,8 +2,31 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def make_bold(function):
+    def wrapper_function():
+        text = function()
+        bold_text = f"<b>{text}</b>"
+        return bold_text
+    return wrapper_function
+
+def make_emphasis(function):
+    def wrapper_function():
+        text = function()
+        emphasized_text = f"<em>{text}</em>"
+        return emphasized_text
+    return wrapper_function
+
+def make_underline(function):
+    def wrapper_function():
+        text = function()
+        underlined_text = f"<u>{text}</u>"
+        return underlined_text
+    return wrapper_function
 
 @app.route('/')
+@make_bold
+@make_emphasis
+@make_underline
 def hello_world():
     return "hello, world!"
 
