94c9d1d9e9871bc11a8ab666753247978c7c58b5
KesterDaniel <kesterdan17@gmail.com>
2023-03-20 21:02:06 +0100

    using converters

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 
@@ -6,5 +7,11 @@ app = Flask(__name__)
 def hello_world():
     return "hello, world!"
 
+
+@app.route('/user/<username>/<int:number>')
+def greet_user(username, number):
+    return f"hello {username}. You are {number} years old"
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
